d9f04cbf19c8674b51b979bab78bde0902b4547c
Aranyos István <aranyos.istvan@kmu.hu>
2025-04-01 18:57:52 +0200

    Feltöltés

---
+++ nyomtatas.js
@@ -0,0 +1,17 @@
+
+function kalkulal() {
+  const szelesseg = parseInt(document.getElementById("szelesseg").value);
+  const magassag = parseInt(document.getElementById("magassag").value);
+  const papir = parseInt(document.getElementById("papirtipus").value);
+
+  const terulet = Math.round((szelesseg * magassag) / 100) / 10; // cm2 -> dm2
+  const koltseg = Math.round(terulet * papir);
+
+  document.getElementById("terulet").innerText = terulet;
+  document.getElementById("papir").innerText = papir;
+  document.getElementById("koltseg").innerText = koltseg;
+
+    document.getElementById("valasz").style.display = "block";
+    document.getElementById("valasz").style.visibility = "visible";
+    
+}
